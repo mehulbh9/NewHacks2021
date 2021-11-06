83b6fa68bcf2d59e9c82b825f4b98e1eb05b9139
Mehul Bhardwaj <84296323+mehulbh9@users.noreply.github.com>
2021-11-06 15:33:39 -0400

    Update assignment.py
    adding classes to assignment

---
+++ assignment.py
@@ -0,0 +1,18 @@
+class Assignment:
+
+    name=""
+    course=""
+    difficulty= 4
+    date = 9
+
+    def __init__(self, name, course):
+        self.name = name
+        self.course
+
+    def difficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def due_date(self, date):
+        self.date = date
+        
+    
