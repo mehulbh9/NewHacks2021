7f64a9870cb11ca3dccd67faafbf673d640a6f5c
Yug Sahu <yug.sahu@mail.utoronto.ca>
2021-11-06 17:27:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def checkInt(str):
 def main():
     namesOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     daysOfTheWeek = []
+    courses = []
     exit = False
 
     for name in namesOfTheWeek:
@@ -33,7 +34,31 @@ def main():
         
         
         if (keyword == "course"): #course
-            pass
+            action = commandList[1]
+            if (action == "add"):
+                while True:
+                    try:
+                        name = str(input("What is the name of the course?"))
+                        break
+                    except ValueError:
+                        print("Invalid value. Please enter a string.")
+                while True:
+                    try: 
+                        difficulty = int(input("What would you say the difficulty of the course is out of 5?"))
+                    except ValueError:
+                        print("Please enter a number between 0 and 5.")
+                    
+                    if (difficulty < 0 or difficulty > 5):
+                        print("Please enter a number between 0 and 5.")
+                    else :
+                        break
+                if (len(courses) == 0):
+                    priority = 0; 
+                else :
+                    print("You currently have the following courses: ")
+                    for course in courses: 
+                        print (course.name)
+                
         elif (keyword == "assignment"): #assignment
             pass
         elif(keyword == "time"): #time
