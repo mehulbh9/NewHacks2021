663db9ce6b1c49f93213e13defbb30481a510a82
gimmix0 <michaelx.0277@gmail.com>
2021-11-06 15:49:19 -0400

    Added main()
    looks nicer

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def makeDay(name):
     day = Day(name)
     return day
 
-daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+def main():
+    daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-for name in daysOfTheWeek:
-    makeDay(name)
+    for name in daysOfTheWeek:
+        makeDay(name)
     
-
+if __name__ == "__main__":
+    main()
