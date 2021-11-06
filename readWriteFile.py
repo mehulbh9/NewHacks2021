0291e7ea9d112a15ea121f9bbf2dc1076c535f73
akwan84 <andrewkwann@gmail.com>
2021-11-06 18:29:50 -0400

    Update readWriteFile.py

--- readWriteFile.py
+++ readWriteFile.py
@@ -12,13 +12,35 @@ def readFile(fileName):
         list2 = l1.split("&")
         temp = Course(list2[0], list2[1], list2[2])
 
-        print("Name =", list2[0])
-        print("Priority =", list2[1])
-        print("Difficulty =", list2[2])
-        print("Assignments =", list2[3])
-        print("Exams =", list2[4])
-
         list3 = list2[3].split(",")
-        print(list3)
+        for assignment in list3:
+            asnAttributes = assignment.split("^")
+            name = asnAttributes[0]
+            course = asnAttributes[1]
+            difficulty = int(asnAttributes[2])
+            date = int(asnAttributes[3])
 
+            tempAssignment = Assignment(name, course)
+            tempAssignment.difficulty = difficulty
+            tempAssignment.date = date
+
+            temp.assignments.append(tempAssignment)
+
+        list4 = list2[4].split(",")
+        for exams in list4:
+            examAttributes = exams.split("^")
+            weight = int(examAttributes[0])
+            importance = int(examAttributes[1])
+            studytime = int(examAttributes[2])
+            course = examAttributes[3]
+            day = int(examAttributes[4])
+
+            tempExam = Exam(weight, importance, studytime, course, day)
+
+            temp.exams.append(tempExam)
+
+
+
+
+#Sample Input: Math1234&1&1&HW1^Math1234^4^9,HW2^Eng1234^3^5&30^3^10^Math1234^8
 readFile("testFile.txt")
