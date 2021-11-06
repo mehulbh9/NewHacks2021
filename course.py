57622553455a60edb2938c43361870ab2ed289e0
Yug Sahu <yug.sahu@mail.utoronto.ca>
2021-11-06 15:29:29 -0400

    Update course.py

--- course.py
+++ course.py
@@ -1,16 +1,19 @@
-class course:
+class Course:
     name = ""
     priority = 0
-    difficulty = 0
+    difficulty = 1
     assignments = []
     exams = []
+
+    def __init__(self, name):
+        self.name = name
 
     def __init__(self, name, priority, difficulty):
         self.name = name 
         self.priority = priority
         self.difficulty = difficulty 
 
+
     def __str__(self) -> str:
         print ("The course ", self.name, " has priority ", self.priority, " and difficulty ", self.difficulty, ".")
 
-    
