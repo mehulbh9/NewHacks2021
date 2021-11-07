2abc175a09b4c8486254b10b5b48963b4903a520
akwan84 <andrewkwann@gmail.com>
2021-11-07 02:07:20 -0500

    Update readWriteDay.py

--- readWriteDay.py
+++ readWriteDay.py
@@ -10,8 +10,21 @@ def readDays(fileName):
         curDay = temp[0]
         time = int(temp[1])
         day_and_totalTime[curDay] = time
+    file.close()
     return day_and_totalTime
+
+def readDays (fileName, days):
+    string = ""
+    for i in range(7):
+        string += days[i].name
+        string += "^"
+        string += str(days[i].timeTotal)
+        string += "*"
+    string = string[0:len(string) - 1]
+
+    file = open(fileName, "w", encoding = "UTF-8")
+    file.write(string)
+    file.close()
 
 #x = readDays("testFile3.txt")
 #print(x)
-
