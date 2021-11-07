8361cd93d82e2a83f86931c4e3a268cf3069ebd3
Yug Sahu <yug.sahu@mail.utoronto.ca>
2021-11-06 20:25:41 -0400

    Update course.py

--- course.py
+++ course.py
@@ -16,12 +16,13 @@ class Course:
 
     def __str__(self) -> str:
         string = "Course " + str(self.name) + "\n"
-        string += "Priority: " + str(self.priority) + "\n"
-        string += "Difficulty: " + str(self.difficulty) + "\n"
+        string += "Priority : " + str(self.priority) + "\n"
+        string += "Difficulty : " + str(self.difficulty) + "\n"
         string += "Upcoming assignments : " + "\n"
         for assignment in self.assignments: 
-            string += assignment.name + " with due date " + str(assignment.duedate.strftime("%a")) + ", " + str(assignment.duedate.strftime("%B")) + " " + str(assignment.duedate.strftime("%d")) + ", " + str(assignment.duedate.strftime("%Y")) + " and completion time : " + str(assignment.time) + "\n"
+            string += str(assignment.name) + " with due date " + str(assignment.duedate.strftime("%a")) + ", " + str(assignment.duedate.strftime("%B")) + " " + str(assignment.duedate.strftime("%d")) + ", " + str(assignment.duedate.strftime("%Y")) + " and completion time : " + str(assignment.time) + "\n"
         string += "Upcoming exams : " + "\n"
         for exam in self.exams: 
-            string += exam.name + " on " + str(exam.date.strftime("%a")) + ", " + str(exam.date.strftime("%B")) + " " + str(exam.date.strftime("%d")) + ", " + str(exam.date.strftime("%Y")) + " and estimated study time : " + str(exam.study_time) + "\n"
+            string += str(exam.name) + " on " + str(exam.date.strftime("%a")) + ", " + str(exam.date.strftime("%B")) + " " + str(exam.date.strftime("%d")) + ", " + str(exam.date.strftime("%Y")) + " and estimated study time : " + str(exam.study_time) + "\n"
+        string += "\n"
         return string
