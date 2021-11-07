aba8da656c136ef8710161f465f4537b394f6aa7
Yug Sahu <yug.sahu@mail.utoronto.ca>
2021-11-06 20:55:51 -0400

    timer.py for pomodoro timer
    If we decide to implement the pomodoro timer...

---
+++ timer.py
@@ -0,0 +1,24 @@
+# import the time module
+import time
+  
+# define the countdown func.
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+      
+    print("Timer is up!")
+  
+  
+# input time in seconds
+m = int(input("Enter the number of minutes : "))
+s = int(input("Enter the number of seconds : "))
+t = m*60 + s
+# t = input("Enter the time in seconds : ")
+  
+# function call
+countdown(int(t))
